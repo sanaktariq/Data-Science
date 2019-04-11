3a43f2e14c635fb4c03a4828f1770ab046cfef9e
sanaktariq <45679625+sanaktariq@users.noreply.github.com>
2019-04-10 18:38:18 -0700

    Add files via upload

---
+++ calc1.py
@@ -0,0 +1,22 @@
+num1 = 10
+num2 = 2
+
+def add(num1,num2):
+   return num1 + num2
+
+def subtract(num1,num2):
+   return num1 - num2
+
+def multiply(num1,num2):
+   return num1 * num2
+
+
+def divide (num1,num2):
+   return num1 / num2
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
