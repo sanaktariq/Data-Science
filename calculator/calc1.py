799d3f793ba3fc90db1af89f1822f849ef9c7ba4
Sana Tariq <sana@procedural.ai>
2019-04-19 20:30:31 -0700

    added if name equal main to circle function

--- calculator/calc1.py
+++ calculator/calc1.py
@@ -13,8 +13,8 @@ def multiply(num1,num2):
 def divide (num1,num2):
    return num1 / num2
 
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
+#print(num1 + num2)
+#print(num1 - num2)
+#print(num1 * num2)
+#print(num1 / num2)
 
